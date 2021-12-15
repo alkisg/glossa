815842e3afac58bdb08a75f0caafffc6014df236
Alkis Georgopoulos <alkisg@gmail.com>
2021-12-15 18:50:30 +0200

    Update

--- glossa.js
+++ glossa.js
@@ -1,18 +1,23 @@
-Δεδομένα = {
-  όνομα: "text",
-  ηλικία: "number"
+Είσοδος = {
+    Όνομα: "text",
+    Βάρος: "number",
+    Ύψος: "number",
 }
 
-function Αλγόριθμος (Δεδομένα, Αποτελέσματα) {
-    όνομα = Δεδομένα.όνομα;
-    ηλικία = Δεδομένα.ηλικία;
-    μήνυμα = "";
+function Αλγόριθμος(Δεδομένα, Αποτελέσματα) {
+    var ΔΜΣ
 
-    if (ηλικία < 81)
-        μήνυμα = "Ο/Η " + όνομα + " είναι ανήλικος"
+    ΔΜΣ = Δεδομένα.Βάρος / Δεδομένα.Ύψος ** 2
+    if (ΔΜΣ < 18.5)
+        Αποτελέσματα.Μήνυμα = "Επικίνδυνα χαμηλό σωματικό βάρος"
+    else if (ΔΜΣ < 25)
+        Αποτελέσματα.Μήνυμα = "Φυσιολογικό βάρος"
+    else if (ΔΜΣ < 30)
+        Αποτελέσματα.Μήνυμα = "Υπέρβαρος"
     else
-        μήνυμα = "Ο/Η " + όνομα + " είναι ενήλικας"
-    Αποτελέσματα.μήνυμα = μήνυμα;
+        Αποτελέσματα.Μήνυμα = "Παχυσαρκία"
+
+    turtle.align = "right";
 }
 
 /*
@@ -42,46 +47,55 @@ function main() {
     outs = {}
 
     var i = 0;
-    for (const [key, value] of Object.entries(Δεδομένα)) {
+    for (const [key, value] of Object.entries(Είσοδος)) {
         ins[key] = document.getElementById(`var_${key}`).value;
     }
     Αλγόριθμος(ins, outs);
-    var ih = ""
-    outputs.innerHTML = `<pre>
-    ${JSON.stringify(outs)}
-    </pre>
-    `
+    var ih = "<table>";  // innerHTML
+    for (const [key, value] of Object.entries(outs)) {
+        ih += `<tr><th><label for="var_${key}">${key}</label></th>
+        <td>${value}</td></tr>`;
+    }
+    ih += "</table>"
+    outputs.innerHTML = ih;
 }
 
 function onLoad() {
-    var ih = "";  // innerHTML
-    for (const [key, value] of Object.entries(Δεδομένα)) {
-        ih += `<label for="var_${key}">${key}</label><br>
-        <input type="${value}" id="var_${key}"><br>`;
+    var ih = "<table>";  // innerHTML
+    for (const [key, value] of Object.entries(Είσοδος)) {
+        ih += `<tr><th><label for="var_${key}">${key}</label></th>
+        <td><input type="${value}" id="var_${key}" name="var_${key}"></td></tr>`;
     }
-    glossajs.innerHTML = `
-    <h1>GlossaJS</h1>
-    <p>Συμπληρώστε τα δεδομένα και πατήστε το κουμπί [Εκτέλεση
-      αλγορίθμου].</p>
-    <p>Το εκπαιδευτικό κομμάτι που προσφέρουμε είναι η ΑΕΠΠ-like βιβλιοθήκη για
-      εκτέλεση αλγορίθμων με Δεδομένα/Αποτελέσματα.</p>
-    <p>Εφόσον γράφουμε ΑΕΠΠ-like javascript, μπορούμε σαν περιβάλλον να
-      χρησιμοποιούμε ο,τιδήποτε υποστηρίζει javascript, π.χ. vscode ή jsfiddle,
-      ενώ με F12 ενεργοποιούμε τον ενσωματωμένο debugger των browsers.</p>
+    ih += "</table>"
+    app.innerHTML = `
     <h2>Δεδομένα</h2>
     <div id="inputs">
       ${ih}
     </div>
     <p>
-    <input type="button" value="Εκτέλεση αλγορίθμου" onclick="main()">
+      <input type="button" value="Εκτέλεση αλγορίθμου" onclick="main()">
+      <input type="button" value="(Προβολή κώδικα)" onclick="window.alert(Αλγόριθμος.toString())">
     </p>
     <h2>Αποτελέσματα</h2>
     <div id="outputs">
-      Εδώ θα εμφανιστούν τα αποτελέσματα.
+      Συμπληρώστε τα δεδομένα και πατήστε [Εκτέλεση αλγορίθμου] για να εμφανιστούν εδώ τα αποτελέσματά του.
     </div>
-    <h2>Περιοχή γραφικών</h2>
-    <p>Εδώ στο μέλλον θα υποστηρίζουμε και canvas για ζωγραφική/χελώνες/ήχο κλπ.</p>
-    <img src="canvas.png">`;
+    <hr>
+    <div style="color: gray">
+    <h3>Περιγραφή περιβάλλοντος GlossaJS</h2>
+    <p>Το εκπαιδευτικό κομμάτι που προσφέρουμε είναι η ΑΕΠΠ-like βιβλιοθήκη για
+      εκτέλεση αλγορίθμων με Δεδομένα/Αποτελέσματα, και αν θέλουμε και πρόσβαση
+      σε logo-like turtle graphics κλπ.</p>
+    <p>Εφόσον γράφουμε ΑΕΠΠ-like javascript, μπορούμε σαν περιβάλλον να
+      χρησιμοποιούμε ο,τιδήποτε υποστηρίζει javascript, π.χ. notepad, vscode
+      ή jsfiddle, ενώ με F12 ενεργοποιούμε τον ενσωματωμένο debugger των
+        browsers.</p>
+    <p>Αφού είμαστε στον browser, ο χειρισμός HTML, γραφικών, ήχου κλπ είναι πανεύκολος, π.χ. turtle.align = "right"</p>
+    </div>
+    <img id="turtle"
+      src="https://download.seaicons.com/icons/martin-berube/flat-animal/256/turtle-icon.png"
+      width="256px">`;
 }
 
-window.onload = onLoad;
+// window.onload = onLoad;
+setTimeout(onLoad, 0);
